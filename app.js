36ac4e4fdff8adb8ec96930dae3d350770e999ef
John Kinya <kingjohnkn@gmail.com>
2023-04-03 00:19:13 +0300

    update app.js

--- app.js
+++ app.js
@@ -8,8 +8,44 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win() {
+    console.log("WON")
+}
+
+function lose() {
+    console.log("LOST")
+}
+
+function draw() {
+    console.log("DRAW")
+}
+
 function game(userChoice) {
-    console.log("you picked    " + userChoice);
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":            
+        case "sp":
+            win();
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+            break;
+    }
+
 }
 
 function main() {
